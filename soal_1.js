7b889f85878c82f5b3ae950d80cea32fd685a1ce
indryrianto <161694673+Indryrianto@users.noreply.github.com>
2024-03-07 15:18:44 +0700

    Update soal_1.js

--- soal_1.js
+++ soal_1.js
@@ -3,7 +3,7 @@ console.log("Bilangan prima dari 1 sampai 1.000 :" )
 for (let l = 2; l <= 1000; l++) {
     let isPrime = true;
 
-    for (let k = 2; k <= Math.sqrt(i); k++) {
+    for (let k = 2; k <= Math.sqrt(l); k++) {
         if (l % k == 0) {
             isPrime = false;
             break;
